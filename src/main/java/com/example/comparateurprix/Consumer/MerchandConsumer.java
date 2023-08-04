1bf68c2ee429d20a20812fa7d707c50349d48dc1
Ahmed Guetat <ahmedguetat62@gmail.com>
2023-08-04 01:51:25 +0100

    * created a MerchandConsumer file that will the csvFilePath and the kafkaTopic to store the data * created a MerchandController * added the necessary variables to parse every data from String[] that I got from csv file

--- src/main/java/com/example/comparateurprix/Consumer/MerchandConsumer.java
+++ src/main/java/com/example/comparateurprix/Consumer/MerchandConsumer.java
@@ -3,7 +3,6 @@ package com.example.comparateurprix.Consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -22,13 +21,26 @@ public class MerchandConsumer {
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
             String line;
             while ((line = br.readLine()) != null) {
-                // Assuming CSV format: sku,name,url,image_url,price,currency, ...
                 String[] data = line.split(",");
                 String sku = data[0];
                 String name = data[1];
-                // etc ...
-
-                String message = sku + "," + name;  // Adjust as needed
+                String url = data[2];
+                String image_url = data[3];
+                String price = data[4];
+                String currency = data[5];
+                String brand = data[6];
+                String category_id = data[7];
+                String variant_group_id = data[8];
+                String size = data[9];
+                String color = data[10];
+                String material = data[11];
+                String model = data[12];
+                String age_group = data[13];
+                String gender = data[14];
+                String operation = data[15];
+                String message = sku + "," + name+ "," + url+ "," + image_url+ "," + price+ "," + currency+ "," + brand
+                        + "," + category_id+ "," + variant_group_id+ "," + size+ "," +
+                        color+ "," + material+ "," + model+ "," + age_group+ "," + gender+ "," + operation;  // Adjust as needed*
                 kafkaTemplate.send(kafkaTopic, sku, message);
             }
         } catch (IOException e) {
